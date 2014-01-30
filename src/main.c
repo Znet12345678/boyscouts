9a6239d5e2b39948912cb9ae53bd34c379664f8b
root <root@ubuntu.ubuntu-domain>
2014-01-29 16:33:40 -0800

    C Project

---
+++ src/main.c
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+int i1;
+int i2;
+int i3;
+int i4;
+int i5;
+int i6;
+int i7;
+int i8;
+int i9;
+int i0;
+int it1;
+int it2;
+int it3;
+int it4;
+int it5;
+int it6;
+int it7;
+int it8;
+int it9;
+int it0;
+//i1 = atoi(c1);
+int main()
+{
+	char input1[10];
+	printf("Welcome to A no-graphical bowling scoreing system.\n");
+	int i;
+	for (i=0; i < 21; i++)
+	{
+	if (i == 1)
+	{
+		char c1[10];
+		fputs("Enter 1st score in frame 1\n",stdout);
+		fflush(stdin);
+		fgets(c1, sizeof c1, stdin);
+		i1 = atoi(c1);
+		
+	}
+	if (i1 < 10)
+	{
+	
+	
+	if (i == 2)
+	{   
+		//printf("%s",c1);
+		char c2[10];
+		fputs("Enter 2nd score in frame 1\n",stdout);
+		fflush(stdin);
+		fgets(c2,sizeof c2,stdin);
+		i2 = atoi(c2);
+	}
+	}	
+	 if (i == 3)
+	{
+		char c3[10];
+		fputs("Enter 1st score in frame 2\n",stdout);
+		fflush(stdin);
+		fgets(c3,sizeof c3,stdin);
+		i3 = atoi(c3);
+	}
+	if (i3 < 10)
+	{
+		
+	
+	if (i == 4)
+	{
+		char c4[10];
+		fputs("Enter 2nd score in frame 2\n",stdout);
+		fflush(stdin);
+		fgets(c4,sizeof c4,stdin);
+		i4 = atoi(c4);
+	}
+	}
+
+	 if (i == 5)
+	{
+		char c5[10];
+		fputs("Enter 1st score in frame 3\n",stdout);
+		fflush(stdin);
+		fgets(c5,sizeof c5,stdin);
+		i5 = atoi(c5);
+	}
+	if (i5 < 10)
+	{
+	if (i == 6)
+	{
+		char c6[10];
+		fputs("Enter 2nd score in frame 3\n",stdout);
+		fflush(stdin);
+		fgets(c6,sizeof c6,stdin);
+		i6 = atoi(c6);
+	}
+
+
+	}
+	 if (i == 7)
+	{
+		char c7[10];
+		fputs("Enter 1st score in frame 4\n",stdout);
+		fflush(stdin);
+		fgets(c7,sizeof c7,stdin);
+		i7 = atoi(c7);
+		//printf("%.d",i7);
+	}
+	if (i7 < 10)
+	{
+	 if (i == 8)
+	{
+		//if (i7 < 10)
+	
+		//{
+		char c8[10];
+		fputs("Enter 2nd score in frame 4\n",stdout);
+		fflush(stdin);
+		fgets(c8,sizeof c8,stdin);
+		i8 = atoi(c8);
+		//}
+		//else
+		//{
+			
+		//}
+		//}
+	}
+	}
+	 if (i == 9)
+	{
+		char c9[10];
+		fputs("Enter 1st score in frame 5\n",stdout);
+		fflush(stdin);
+		fgets(c9,sizeof c9,stdin);
+		i9 = atoi(c9);
+	}
+	if (i9 < 10)
+	{
+	 if (i == 10)
+	{
+		char c10[10];
+		fputs("Enter 2nd score in frame 5\n",stdout);
+		fflush(stdin);
+		fgets(c10,sizeof c10,stdin);
+		i0 = atoi(c10);
+	}
+	}
+	//else if (i > 3 && i < 11)
+	//{
+	//	
+	//
+	//}
+ if (i == 11)
+	{
+		char c11[10];
+		fputs("Enter 1st score in frame 6\n",stdout);
+		fflush(stdin);
+		fgets(c11,sizeof c11,stdin);
+		it1 = atoi(c11);
+	}
+	if (it1 < 10)
+	{
+	if (i == 12)
+	{
+		char c12[10];
+		fputs("Enter 2nd score in frame 6\n",stdout);
+		fflush(stdin);
+		fgets(c12, sizeof c12, stdin);
+		it2 = atoi(c12);
+	}
+	}
+	if (i == 13)
+	{
+		char c13[10];
+		fputs("Enter 1st score in frame 7\n",stdout);
+		fflush(stdin);
+		fgets(c13,sizeof c13,stdin);
+		it3 = atoi(c13);
+	}
+	//else if (i > 13)
+	//{
+		// 
+	//}
+	
+		//if (i == 1)
+		//{
+		//int i2;
+		//char input1[100];
+	//	printf("Enter ");
+	//	printf("%.d",i);
+	//	printf("st score of frame");
+	//	fputs("",stdout);
+		//fflush(stdin);
+	//	fgets(input1, sizeof input1, stdin);
+//		}
+//		else if (i == 2)
+//		{
+//		char input1[100];
+//		printf("Enter ");
+//		printf("%.d",i);
+//		fputs("nd score \n",stdout);
+//		
+//		fflush(stdin);
+//		fgets(input1, sizeof input1, stdin);
+//		}
+//		else if (i == 3)
+//		{
+//					char input1[100];
+//		printf("Enter ");
+//		printf("%.d",i);
+//		fputs("rd score \n",stdout);
+//		
+//		fflush(stdin);
+//		fgets(input1, sizeof input1, stdin);
+//		}
+//		else if (i > 3)
+//		{
+//					char input1[100];
+//		printf("Enter "); 
+//		printf("%.d",i);
+//		fputs("th score \n",stdout);
+//		
+//		fflush(stdin);
+//		fgets(input1, sizeof input1, stdin);
+//		}
+	
+		
+			}
+			}
+
+	
